2d82f29bee44cd2a581728d5ffb068117142483f
Taylor Ambrose <taykesby@gmail.com>
2023-03-19 14:51:22 -0400

    lab complete

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,10 @@
 def oxford_comma(items):
-    return None
+    if len(items) > 2:
+        the_string = ", ".join(items[:-1]) + ", and " + items[-1]
+        return the_string
+    elif len(items) == 2:
+        the_string = " and ".join(items)
+        return the_string
+    else:
+        string = "".join(items)
+        return string
